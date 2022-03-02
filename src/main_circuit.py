39920282b3803661474fd52dfee852f0bd64dc80
Akash Kundu <kunduaku339@gmail.com>
2022-03-02 18:32:40 +0100

    up

--- src/main_circuit.py
+++ src/main_circuit.py
@@ -1,6 +1,30 @@
 from qiskit import *
 from qiskit import Aer
 import numpy as np
+from itertools import combinations
+
+
+def setpartition(iterable, n=2):
+    iterable = list(iterable)
+    partitions = combinations(combinations(iterable, n), r=int(len(iterable)/n))
+    for partition in partitions:
+        seen = set()
+        for group in partition:
+            if seen.intersection(group):
+                break
+            seen.update(group)
+        else:
+            yield partition
+
+
+def setpartition_to_list(setpartition):
+    qubit_partitions = []
+    for el in list(setpartition):
+        small_list = []
+        for no in range(len(el)):
+            small_list.append( list(el[no]) )
+        qubit_partitions.append(small_list)
+    return qubit_partitions
 
 
 def cbell():
@@ -15,7 +39,7 @@ def cbell():
     return gate
 
 
-def aritra_dar_causality( aritra_dar_dimension ):
+def aritra_dar_causality( aritra_dar_dimension , qubit_partitions ):
     """
     
     aritra_dar_dimension = number of qubits in causal circuit (Bell connections),
@@ -23,24 +47,26 @@ def aritra_dar_causality( aritra_dar_dimension ):
     control_qubits = list of control qubits
 
     """
-    qc = QuantumCircuit( aritra_dar_dimension + np.ceil( np.log2( aritra_dar_dimension ) ) )
-
-    target_qubits_total = [ [ 0, 1 ], [ 2, 3 ] , [ 0, 2 ], [ 1, 3 ], [ 0, 3 ], [ 1, 2 ] ] #TODO: Tamal dar karshaji
-    
+    target = np.ceil( np.log2( aritra_dar_dimension ) )
+    qc = QuantumCircuit( aritra_dar_dimension + target )
+    target_qubits_total = qubit_partitions #TODO: Tamal dar karshaji
     control_qubits = []
+
     for el in range(aritra_dar_dimension//2):
         control_qubits.append( aritra_dar_dimension + el )
-
-    for target_qubits in target_qubits_total:
-        total = control_qubits + target_qubits
-        qc.append( cbell(), total )
-        qc.barrier()
+    
+    for target_no, target_qubits in enumerate(target_qubits_total):
+        for target in target_qubits:
+            total = control_qubits + target
+            qc.append( cbell(), total )
+            qc.barrier()
     
     return qc
 
 
-print(aritra_dar_causality(4))
-
-qc = QuantumCircuit(4)
-
-# qc.cent()
+if __name__ == "__main__":
+    aritra_dar_dimension = 4
+    partition = list(setpartition(list(range(aritra_dar_dimension))))
+    qubit_partitions = setpartition_to_list(partition)
+    circuit = aritra_dar_causality( aritra_dar_dimension , qubit_partitions )
+    print(circuit)
