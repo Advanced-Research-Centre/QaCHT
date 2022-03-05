c6518acafb6eb705f4d19989fc3af1d34d1ba3de
Aqasch <58222498+Aqasch@users.noreply.github.com>
2022-03-05 06:35:59 +0100

    Algorithm: done

--- src/main_circuit.py
+++ src/main_circuit.py
@@ -18,6 +18,15 @@ def setpartition(iterable, n=2):
 
 
 def setpartition_to_list(setpartition):
+    """
+    Input:
+    ------
+    setpartition: A permutation of causally connected qubits in form of sets.
+    
+    Output:
+    A conversion to list of the set of permutations. 
+
+    """
     qubit_partitions = []
     for el in list(setpartition):
         small_list = []
@@ -26,45 +35,84 @@ def setpartition_to_list(setpartition):
         qubit_partitions.append(small_list)
     return qubit_partitions
 
-
-def cbell():
+def circuit_subroutine(qc, control_qubits, target_qubits, bit):
     """
+    Inputs:
+    -------
+    qc = Quantum circuit.
+    control_qubits = list of control qubits.
+    target_qubits = list of target qubits.
+    bit = bit value corresponding to the permutations i.e. for 0: '00'
+    
+    Outputs:
+    --------
+    Aritra dar causality hypothesis circuit 
+
+    """
+    for target in target_qubits:
+        total = control_qubits + target
+        for i in range(len(bit)):
+            if bit[i] == '1':
+                qc.x([control_qubits[0] + i])
+        qc.append( cbell(len(control_qubits)), total )
+        for i in range(len(bit)):
+            if bit[i] == '1':
+                qc.x([control_qubits[0] + i])
+        qc.barrier()
+    return qc
+
+
+def cbell(control_no):
+    """
+    Input:
+    ------
+    control_no = numebr of control qubits
+    Output:
+    -------
     return Bell's Unitary.
     """
-    n_ent = 2
-    qc_ent = QuantumCircuit(n_ent)
+    qc_ent = QuantumCircuit(2)
     qc_ent.h([0])
     qc_ent.cx([0], [1])
-    gate = qc_ent.to_gate(label = 'Max. Ent. gate').control(2)
+    gate = qc_ent.to_gate(label = 'Bell Unitary').control(control_no)
     return gate
 
 
 def aritra_dar_causality( aritra_dar_dimension , qubit_partitions ):
     """
-    
+    Input:
+    ------
     aritra_dar_dimension = number of qubits in causal circuit (Bell connections),
     target_qubits = list of target qubits
     control_qubits = list of control qubits
-
+    
+    Output:
+    -------
+    Aritra dar causality hypothesis circuit
     """
-    target = np.ceil( np.log2( aritra_dar_dimension ) )
-    qc = QuantumCircuit( aritra_dar_dimension + target )
-    target_qubits_total = qubit_partitions #TODO: Tamal dar karshaji
+    control_no = int( np.ceil( np.log2( aritra_dar_dimension ) ) )
+    qc = QuantumCircuit( aritra_dar_dimension + control_no )
+    target_qubits_total = qubit_partitions
     control_qubits = []
-
-    for el in range(aritra_dar_dimension//2):
+    for el in range(control_no):
         control_qubits.append( aritra_dar_dimension + el )
     
-    for target_no, target_qubits in enumerate(target_qubits_total):
-        for target in target_qubits:
-            total = control_qubits + target
-            qc.append( cbell(), total )
-            qc.barrier()
-    
+    num = 0
+    t = target_qubits_total[0][0]
+    for i in range(len(target_qubits_total)):
+        if t == target_qubits_total[i][0]:
+            bit = format(num, f'0{control_no}b')
+            circuit_subroutine(qc, control_qubits, control_no, target_qubits_total[i], bit)
+        else:
+            t = target_qubits_total[i][0]
+            num +=1
+            bit = format(num, f'0{control_no}b')
+            circuit_subroutine(qc, control_qubits, control_no, target_qubits_total[i], bit)
     return qc
 
 
 if __name__ == "__main__":
+
     aritra_dar_dimension = 4
     partition = list(setpartition(list(range(aritra_dar_dimension))))
     qubit_partitions = setpartition_to_list(partition)
